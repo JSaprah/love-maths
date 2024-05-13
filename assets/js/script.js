36d349469a5c7748114a502499dac0a007c81dc4
Jasmeen Saprah <j.sapraa@gmail.com>
2024-05-13 16:52:21 +0000

    Add even listeners to document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+//Add even listers - - Code to be executed when the page has finished loading and Code to be executed when the user clicks a button
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "Submit") {
+                alert("You clicked submit!");
+            }
+            else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`)
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
