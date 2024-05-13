e26ce7d6a00477bba56ca1ab634740f9de1f350f
Jasmeen Saprah <j.sapraa@gmail.com>
2024-05-13 17:07:40 +0000

    Add rungame function to generate two random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "Submit") {
                 alert("You clicked submit!");
-            }
+       }
             else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`)
@@ -14,8 +14,15 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 })
 
+/**
+ * The main game loop, called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
 
+    //Create two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
